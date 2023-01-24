13be07710dc7c77bbfd70ace00457fa0719cbe22
extra-300s <1446457778@qq.com>
2023-01-24 16:53:53 +0800

    Site updated: 2023-01-24 16:53:52

--- js/music.js
+++ js/music.js
@@ -1,7 +1,6 @@
 const ap = new APlayer({
     container: document.getElementById('player'),   //播放器HTML容器元素
-    fixed: true,
-    mini: false,        //迷你模式开关
+    fixed: true,    //吸底模式
     autoplay: true,    //自动播放开关（Chromium现已禁止自动播放）
     //theme: '#FADFA3',   //主题色
     loop: 'all',        //音频循环播放, 可选值: 'all'全部循环, 'one'单曲循环, 'none'不循环
@@ -11,7 +10,7 @@ const ap = new APlayer({
     mutex: true,        //互斥，阻止多个播放器同时播放，当前播放器播放时暂停其他播放器
     listFolded: false,  //列表默认折叠
     listMaxHeight: 90,  //列表最大高度
-    lrcType: 3,         //歌词传递方式
+    //lrcType: 3,         //歌词传递方式
     audio: [            ////音频信息,包含以下
         {
             name: '青春コンプレックス',          //音频名称
